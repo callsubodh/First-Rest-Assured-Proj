c21826d3a10ddc8564a40d25261396c8f8e61426
callsubodh <callsubodh@gmail.com>
2024-01-27 22:32:51 -0800

    This is my second commit

---
+++ src/test/java/appTest/AppTest.java
@@ -0,0 +1,13 @@
+package appTest;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+	public void testone() {
+		
+		System.out.println("This is my first new java class..");
+	}
+
+}
